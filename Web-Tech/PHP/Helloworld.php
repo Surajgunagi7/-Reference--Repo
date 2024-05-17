95faee2a782311a8f3151d1401763b23ebf6f3ed
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-05-17 18:59:28 +0530

    wdrW

---
+++ Web-Tech/PHP/Helloworld.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Welcome to php!!! <br/>" ;
+    print("Hello from print<br/>");
+    printf("Hello from printf");
+
+    $name = "SDM College";
+    $num = 455;
+    $floatNum = 2.3;
+    echo "<h2>Variables in PHP</h2><br/>";
+    echo "First variable: ".$name."<br/>";
+    echo "Second variable: ".$num."<br/>";
+    echo "Third variable: ".$floatNum;
+
+?>
