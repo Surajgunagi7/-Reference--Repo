975b697220f3d54ddc7121abeb45098c8b0f91fb
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-06-02 19:49:40 +0530

    Update Basic.java

--- Java/Java Lab - Programs/02. Maths/Basic.java
+++ Java/Java Lab - Programs/02. Maths/Basic.java
@@ -1,4 +1,4 @@
-package sdmcet.cse.oops.basicMath;
+package oops.basicMath;
 
 public class Basic {
 
@@ -6,22 +6,17 @@ public class Basic {
 		if (number <= 1)
 			return false;
 
-		if (number > 0 && number <= 3)
-			return true;
-
-		if (number % 2 == 0 || number % 3 == 0)
-			return false;
-
-		for (int i = 5; i * i <= number; i = i + 6) {
-			if (number % i == 0 || number % (i + 2) == 0)
+		for (int i = 2; i < number / 2; i++) {
+			if (number % 2 == 0) {
 				return false;
+			}
 		}
 		return true;
 	}
 
 	public int sumCalculate(int number) {
 		int sum = 0;
-		
+
 		while (number != 0) {
 			int digit = number % 10;
 			sum += digit;
