2e13bc30687e0e1d35913b968e84812586d42731
Suraj Gunagi <surajgunagi7025@gmail.com>
2024-06-03 00:50:54 +0530

    Update Advanced.java

--- Java/Java Lab - Programs/02. Maths/Advanced.java
+++ Java/Java Lab - Programs/02. Maths/Advanced.java
@@ -15,7 +15,7 @@ public class Advanced {
 		int sum = 0;
 		for (int i = 0; i < arr.length; i++) {
 			for (int j = 0; j < arr.length; j++) {
-				if (arr[i] == arr[j]) {
+				if (i == j) {
 					sum = sum + arr[i][i];
 				}
 			}
